d8eb9bd3308dd2d5253fe61abbd9e3b789c2e0c1
abhi1401 <abhishek.mvr@outlook.com>
2018-03-11 23:22:46 +0530

    Added Console App for performing processing

--- Aleida1/Controllers/AnalyticsController.cs
+++ Aleida1/Controllers/AnalyticsController.cs
@@ -6,14 +6,72 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Aleida1.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace Aleida1.Controllers
 {
     public class AnalyticsController : Controller
     {
         private DataServerContext ds = new DataServerContext();
-        //[Authorize]
+        private List<IPAddress> LanIP;
 
+        //Check whether the ip is suspected or not from SuspectedIPs.csv file
+        bool IsSuspected(IPAddress ip)
+        {
+            var lines = System.IO.File.ReadLines("SuspectedIPs.csv");
+            foreach (var line in lines)
+            {
+                if(line.Equals(ip))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Class for each ip connections
+        private class Connection
+        {
+            IPAddress lanip;
+            IPAddress swanip;
+        }
+
+        //Class for info on each connections
+        private class ConnectionInfo
+        {
+            int start;
+            int end;
+            int[] acthours = new int[24];
+
+            public int acthour()
+            {
+                return acthours.Sum();
+            }
+        }
+
+        class DPLayer
+        {
+            Connection LanIP2WanIP;
+            float ActHour, ActRate, ActWeight, FailHour, FailRate, FailWeight, FailFlow, FailMatch, NoExist, DPortSum;
+        }
+
+        //Dictionary for all connection_info
+        Dictionary<Connection, ConnectionInfo> Connections;
+
+        private void ReadRawData()
+        {
+            String pattern = "s*,s*,s*,s*,s*";
+            String[] tokens = new String[5];
+            var lines = System.IO.File.ReadLines("RawData.csv");
+            foreach (var line in lines)
+            {
+                tokens = Regex.Split(line,pattern);
+            }
+
+        }
+
+        //[Authorize]
         public IActionResult Index()
         {
             Console.WriteLine("Index method in Analytics");
@@ -25,6 +83,8 @@ namespace Aleida1.Controllers
             Pcdetails pc = ds.Pcdetails.Find(id);
             return View(pc);
         }
+
+
 
     }
 }
---
+++ AleidaConsole/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace AleidaConsole
+{
+
+    static class Program
+    {
+        static void Main()
+        {
+            Console.Write("Starting Aleida\n");
+            using (var progress = new ProgressBar())
+            { 
+                for (int i = 0; i <= 100; i++)
+                {
+                    progress.Report((double)i / 100);
+                    Thread.Sleep(20);
+                }
+            }
+            Console.WriteLine("Done.");
+            ReadRawData();
+        }
+
+    //Check whether the ip is suspected or not from SuspectedIPs.csv file
+    private static bool IsSuspected(String ip)
+    {
+        if (ip == null)
+        {
+            throw new ArgumentNullException(nameof(ip));
+        }
+        var lines = System.IO.File.ReadLines("SuspectedIPs.csv");
+        foreach (var line in lines)
+        {
+            if (line.Equals(ip))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+        //Class for each ip connections
+        private class Connection
+        {
+            public String lanip;
+            public String swanip;
+            public Connection(String lanip, String swanip)
+            {
+                this.lanip = lanip;
+                this.swanip = swanip;
+            }
+            
+            public override string ToString()
+            {
+                return lanip;
+            }
+
+        }
+
+        //Class for info on each connections
+        private class ConnectionInfo
+        {
+            public int start;
+            public int end;
+            public int[] acthours = new int[24];
+            public ConnectionInfo()
+            { }
+        }
+
+        //Dictionary for all connection_info
+        static Dictionary<Connection, ConnectionInfo> Connections;
+        static Dictionary<Connection, ConnectionInfo> temp;
+
+
+        class DPLayer
+        {
+            Connection con { get; set; }
+            public float ActHour()
+            {
+                var xconn = from conn in Connections where conn.Key.ToString() == con.lanip select conn;
+                return xconn.Max(x=>x.Value.acthours.Sum());
+            }
+                //, ActRate, ActWeight, FailHour, FailRate, FailWeight, FailFlow, FailMatch, NoExist, DPortSum;
+
+        }
+
+
+
+        private static void ReadRawData()
+        {
+            Connection key;
+            ConnectionInfo value;
+            int hour;
+            //String pattern = "s*,s*,s*,s*,s*";
+            //String[] token = new String[5];
+            //var lines = System.IO.File.ReadLines("RawData.csv");
+            //foreach (var line in lines)
+            //{
+            //    token = Regex.Split(line, pattern);
+            //    Console.WriteLine(token[0] + " : " + " source : " + token[0] + " dest : " + token[0] + "\n");
+
+            StreamReader readFile = new StreamReader("RawData.cs");
+            string line;
+            string[] row;
+            var tokens;
+            readFile.ReadLine();
+            while ((line = readFile.ReadLine()) != null)
+            {
+                row = line.Split(',');
+            }
+                hour = token[0]);
+                key = new Connection(token[1], token[2]);
+                if (Connections.TryGetValue(key, out value))
+                {
+                    value.acthours[hour]++;
+                    if (value.start < hour)
+                    {
+                        value.start = hour;   
+                    }
+                    if(value.end > hour)
+                    {
+                        value.end = hour;
+                    }
+                    Connections[key] = value;
+                }
+                else
+                {
+                    value = new ConnectionInfo();
+                    value.start = hour;
+                    value.end = hour;
+                    value.acthours[hour]++;
+                    Connections.Add(key, value);
+                }
+            }
+        readFile.Close();
+
+
+    }
+
+}
+
